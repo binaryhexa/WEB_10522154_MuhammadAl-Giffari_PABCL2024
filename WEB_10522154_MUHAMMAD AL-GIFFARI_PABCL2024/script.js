916f59c30f7f6c04fa306c78aa1d418fa65b36a3
binaryhexa <116893563+binaryhexa@users.noreply.github.com>
2024-01-16 16:27:25 +0700

    Update script.js

--- WEB_10522154_MUHAMMAD AL-GIFFARI_PABCL2024/script.js
+++ WEB_10522154_MUHAMMAD AL-GIFFARI_PABCL2024/script.js
@@ -1,3 +1,4 @@
+// Buat sebuah fungsi untuk tombol Convert yang didalamnya mempunyai variable dan rumus untuk mengubah Centimeter ke Inch, Meter, dan Feet
 function convert() {
   var cm = document.getElementById("cm").value;
   var convert = document.getElementById("pilihan").value;
@@ -9,6 +10,7 @@ function convert() {
   var hasilFeet = feet * 0.032804;
   var result = document.getElementById("result");
 
+  // Membuat sebuah kondisi dimana jika kita tidak menginputkan nilai, maka akan muncul alert dan kita tidak bisa mengkonversi nilai sebelum kita menginputkan sebuah angka
   if (cm <= 0) {
     alert("Please Enter the Correct Number!");
   } else if (convert == "inch") {
@@ -18,4 +20,5 @@ function convert() {
   } else if (convert == "feet") {
     result.innerHTML ="<br><br>" + "Your body height is " + hasilFeet.toFixed(2) + " Feet";
   }
+  // toFixed(nilai) digunakan untuk membulatkan ke tempat desimal tertentu. Dalam kasus ini, toFixed(2) digunakan untku menempatkan 2 angka di belakang koma.
 }
