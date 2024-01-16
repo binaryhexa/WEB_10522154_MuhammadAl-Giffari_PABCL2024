b3442140f10c6d8b80b6af74ec807a13db211169
binaryhexa <116893563+binaryhexa@users.noreply.github.com>
2024-01-16 15:32:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function convert() {
+  var cm = document.getElementById("cm").value;
+  var convert = document.getElementById("pilihan").value;
+  var inch = document.getElementById("cm").value;
+  var hasilInch = inch * 0.393701;
+  var meter = document.getElementById("cm").value;
+  var hasilMeter = meter / 100;
+  var feet = document.getElementById("cm").value;
+  var hasilFeet = feet * 0.032804;
+  var result = document.getElementById("result");
+
+  if (cm <= 0) {
+    alert("Please Enter the Correct Number!");
+  } else if (convert == "inch") {
+    result.innerHTML ="<br><br>" + "Your body height is " + hasilInch.toFixed(2) + " Inch";
+  } else if (convert == "meter") {
+    result.innerHTML ="<br><br>" + "Your body height is " + hasilMeter.toFixed(2) + " Meter";
+  } else if (convert == "feet") {
+    result.innerHTML ="<br><br>" + "Your body height is " + hasilFeet.toFixed(2) + " Feet";
+  }
+}
